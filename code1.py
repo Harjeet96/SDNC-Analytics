fbd158999e50707383ef394d3040256e9281f9ce
Harjeet96 <56324465+Harjeet96@users.noreply.github.com>
2019-12-07 14:56:28 -0500

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,6 +1,6 @@
 # assigning two series to s1 and s2 
 s1 = pd.Series([1,2]) 
-s2 = pd.Series(["Ashish", "Sid"]) 
+s2 = pd.Series(["Har", "Jeet"]) 
 # framing series objects into data 
 df = pd.DataFrame([s1,s2]) 
 # show the data frame 
@@ -8,7 +8,7 @@ df
 
 # data framing in another way 
 # taking index and column values 
-dframe = pd.DataFrame([[1,2],["Ashish", "Sid"]], 
+dframe = pd.DataFrame([[1,2],["Har", "Jeet"]], 
 		index=["r1", "r2"], 
 		columns=["c1", "c2"]) 
 dframe 
@@ -16,6 +16,6 @@ dframe
 # framing in another way 
 # dict-like container 
 dframe = pd.DataFrame({ 
-		"c1": [1, "Ashish"], 
-		"c2": [2, "Sid"]}) 
+		"c1": [1, "Har"], 
+		"c2": [2, "Jeet"]}) 
 dframe 
