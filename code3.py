7af370fee60ebf74283b758ae9ec9bbf87b79aa6
Harjeet96 <56324465+Harjeet96@users.noreply.github.com>
2019-12-07 15:10:58 -0500

    Create code3.py

---
+++ code3.py
@@ -0,0 +1,6 @@
+# prints first 5 rows and every column which replicates df.head() 
+df.iloc[0:5,:] 
+# prints entire rows and columns 
+df.iloc[:,:] 
+# prints from 5th rows and first 5 columns 
+df.iloc[5:,:5] 
