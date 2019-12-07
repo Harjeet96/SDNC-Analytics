e4ffaec43df52ef8bc5edf96db5bc6b2789560bc
Harjeet96 <56324465+Harjeet96@users.noreply.github.com>
2019-12-07 14:46:38 -0500

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,21 @@
+# assigning two series to s1 and s2 
+s1 = pd.Series([1,2]) 
+s2 = pd.Series(["Ashish", "Sid"]) 
+# framing series objects into data 
+df = pd.DataFrame([s1,s2]) 
+# show the data frame 
+df 
+
+# data framing in another way 
+# taking index and column values 
+dframe = pd.DataFrame([[1,2],["Ashish", "Sid"]], 
+		index=["r1", "r2"], 
+		columns=["c1", "c2"]) 
+dframe 
+
+# framing in another way 
+# dict-like container 
+dframe = pd.DataFrame({ 
+		"c1": [1, "Ashish"], 
+		"c2": [2, "Sid"]}) 
+dframe 
